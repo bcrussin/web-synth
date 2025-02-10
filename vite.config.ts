7270e2bd7b57f8621ba2aed80e5fdf6659fae766
Barrett Crusse <cvillebear@gmail.com>
2025-02-10 13:40:46 -0500

    Test Vite base fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://bcrussin.github.io/web-synth',
+  base: '/web-synth/',
   plugins: [
     vue(),
     vueDevTools(),
